b325ef10d128252c4c8f616fe56033747f57475f
Sai Teja Macharla <18657211+saitej123@users.noreply.github.com>
2020-05-31 01:16:46 +0530

    Add files via upload

---
+++ setup.sh
@@ -0,0 +1,13 @@
+mkdir -p ~/.streamlit/
+
+echo "\
+[general]\n\
+email = \"macharlasaiteha@gmail.com\"\n\
+" > ~/.streamlit/credentials.toml
+
+echo "\
+[server]\n\
+headless = true\n\
+enableCORS=false\n\
+port = $PORT\n\
+" > ~/.streamlit/config.toml
